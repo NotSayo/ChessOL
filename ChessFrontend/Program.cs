21411a48134d0e478d9428b8fd0749033037cafc
Leonhard-Neuhold <leonhard.neuhold@outlook.de>
2025-03-19 19:00:19 +0100

    some frontend changes, fighting with mudblazor css

--- ChessFrontend/Program.cs
+++ ChessFrontend/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ChessFrontend;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices(options =>
+    options.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
